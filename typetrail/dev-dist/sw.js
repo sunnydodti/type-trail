b614649fdabc4baf403909475b30570fe3c81bc8
Sunny Dodti <sunnydodti@gmail.com>
2025-06-04 03:14:37 +0530

    test

--- typetrail/dev-dist/sw.js
+++ typetrail/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bdi0736dosg"
+    "revision": "0.lfds56dq0vo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
